4da77a7bccbb43b0bf71f19c1388f9d09dbfeaea
Kristin Sztengel <kcsztengel@gmail.com>
2016-08-25 15:40:38 -0600

    error works

--- js/app.js
+++ js/app.js
@@ -1,21 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // console.log('doc is loaded!')
 
-  let $screen = $('#screen')
+  var $screen = $('#screen')
 
-$("#buttons-container").on("click", function(){
-  let currentNumber = ($(event.target).text())
-  $screen.append(currentNumber)
+$("span").on("click", function(){
+  let currentNumber = $(this).text()
+  if (currentNumber === 'C'){
+      $screen.text('');
+  } else {
+      $screen.append(currentNumber)
+  }
 });
 
 $(".operator").on("click", function(){
-  let currentOperator = $(event.target).text()
+  let currentOperator = $(this).text()
 });
-//'C' appends up on last "clear"
-$('#clear').on('click', function() {
-  $screen.text('');
-  });
-
 
 $('#equals').on('click', function() {
  var equation = $screen.text();
@@ -31,58 +29,37 @@ $('#equals').on('click', function() {
  firstNumber = parseInt(equation.substring(0, add))
  secondNumber = parseInt(equation.substring(add + 1, equation.length))
  total = firstNumber + secondNumber
-
  }
 
- if(subtract !== -1){
+ if (subtract !== -1){
    firstNumber = parseInt(equation.substring(0, subtract))
    secondNumber = parseInt(equation.substring(subtract + 1, equation.length))
    total = firstNumber - secondNumber
-
  }
 
- if(multiply !== -1){
+ if (multiply !== -1){
    firstNumber = parseInt(equation.substring(0, multiply))
    secondNumber = parseInt(equation.substring(multiply + 1, equation.length))
    total = firstNumber * secondNumber
-
  }
 
  if (divide!== -1){
     firstNumber = parseInt(equation.substring(0, divide))
     secondNumber = parseInt(equation.substring(divide + 1, equation.length))
     total = firstNumber/secondNumber
-    // $screen.append(total)
   }
 
-if (typeof total === "number" && total === total){
+  if (divide!== -1){
+     firstNumber = parseInt(equation.substring(0, divide))
+     secondNumber = 0
+     $screen.text("error")
+   }
+
+  if (typeof total === "number" && total === total && total !== Infinity){
     $screen.text(total)
   }
-else if(typeof total !== "number"){
-  $screen.text("error")
-}
+  else if(typeof total !== "number"){
+     $screen.text("error")
+  }
   })
 });
-
-//   $(".operator").on("click", function(){
-//     let currentOperator = $(event.target).text()
-//   if(number1){
-//     number2 = parseInt($('screen').text(),10)
-//   } else {
-//     number1 = parseInt($('screen').text(),10)
-//   }
-//   console.log("currentOperator", currentOperator);
-//   console.log("2", number2)
-//
-//   if (number1 && number2 && currentOperator){
-//
-//   if (currentOperator === '+'){
-//     answer = (number1 + number2)
-//   } else if (currentOperator === '-'){
-//     answer = (number1-number2)
-//   } else if (currentOperator === "x"){
-//     answer = (number1 * number2)
-//   } else if (currentOperator === '÷'){
-//     answer = number1/number2
-//   }
-// }
